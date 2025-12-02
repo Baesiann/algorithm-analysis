6485624ba4dc427bdfbc1588b6a393d5b5b005d2
Baesiann <k3nn3thburk3@gmail.com>
2025-12-02 14:26:35 -0500

    list gen now adapts to array size

--- src/list_gen.py
+++ src/list_gen.py
@@ -6,13 +6,13 @@ random.seed(0)
 
 # Creates a list of size len, returns unique random numbers
 def random_list(len):
-  arr = random.sample(range(0, 10000), len)
+  arr = random.sample(range(0, len*10), len)
   return arr
 
 # Creates a list where all elements are at most k positions away from correct pos
 # Uses a min heap
 def nearly_sorted_list(len, k):
-  arr = random.sample(range(0, 10000), len)
+  arr = random.sample(range(0, len*10), len)
   pq = []
   # Push first k elements
   for i in range(k):
@@ -34,19 +34,12 @@ def nearly_sorted_list(len, k):
 
 # Creates a reversely sorted list with unique numbers
 def reverse_list(len):
-  arr = random.sample(range(0, 15000), len)
+  arr = random.sample(range(0, len*15), len)
   arr.sort()
   arr.reverse()
   return arr
 
 # Creates a list of size len, includes logic for heavy duplicates
 def heavy_duplicate_list(len):
-  if len <= 10:
-    arr = [random.randint(1, 3) for _ in range(len)]
-  elif len <=  100:
-    arr = [random.randint(1, 10) for _ in range(len)]
-  elif len <= 1000:
-    arr = [random.randint(1, 100) for _ in range(len)]
-  else:
-    arr = [random.randint(1,500) for _ in range(len)]
+  arr = [random.randint(1, len//20) for _ in range(len)]
   return arr
